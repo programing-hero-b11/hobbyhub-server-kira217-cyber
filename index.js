669e92281b9950929346ad779ab73e8360fc2dae
kira217-cyber <gamingkira217@gmail.com>
2025-05-20 20:26:02 +0600

    added delete method in the mongodb

--- index.js
+++ index.js
@@ -71,6 +71,14 @@ async function run() {
 
     })
 
+    // delete method
+    app.delete('/groups/:id', async(req,res)=>{
+      const id =req.params.id;
+      const query = {_id:new ObjectId(id)};
+      const result = await groupsCollection.deleteOne(query);
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
